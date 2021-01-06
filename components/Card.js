28aa5cb104701d7f88b4448d78cffc955df37f6a
e.petrova <e.petrova@int.robotbull.com>
2021-01-06 04:54:21 +0300

    card styles

--- components/Card.js
+++ components/Card.js
@@ -1,21 +1,25 @@
 import React from "react";
 const Card = ({wish}) => {
-	const {title, desc, link, img, price, status, date} = wish;
+	const {title, desc, link, linkDesc = "Ссылка", img, price, status, date} = wish;
 	console.log(img)
 	return (
 		<div
-			className="w-60 h-80 shadow-md rounded-md p-6 m-6">
+			className="w-auto h-80 shadow-md rounded-md p-4 m-6">
 			{img ?
-				<img
-					src={img}
-					alt={title}
-				/>
+				<div className="flex justify-center mb-1">
+					<img
+						src={img}
+						alt={title}
+						className="block mx-auto h-32 flex-shrink"
+					/>
+				</div>
 
 			:
-				<div className="block mx-auto h-24 sm:mx-0 sm:flex-shrink-0 bg-magic-wand bg-no-repeat bg-center"/>
+				<div className="mb-1 block mx-auto h-24 sm:mx-0 sm:flex-shrink-0 bg-magic-wand bg-no-repeat bg-center"/>
 			}
-			<p>{title}</p>
-			<p>{desc}</p>
+			{link && <a href={link} target="_blank" className="text-blue-700 text-xs">{linkDesc}</a>}
+			<p className="font-semibold text-lg">{title}</p>
+			<p className="text-sm">{desc}</p>
 		</div>
 	)
 }
