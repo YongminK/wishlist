d1d85333747d62c11c8552dffca65f4ebfd37a8d
e.petrova <e.petrova@int.robotbull.com>
2021-01-06 04:53:57 +0300

    simple layout with header

---
+++ layouts/MainLayout/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+	return (
+		<div className="h-14 bg-blue-700 px-5 py-1">
+			<p className="text-white text-xl leading-loose h-full">My WishList</p>
+		</div>
+	)
+}
+
+export default Header;
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,21 @@
 import Card from "../components/Card";
 import {WISHLIST} from "../wishlist/wishes";
+import MainLayout from "../layouts/MainLayout/MainLayoyt";
 
 function HomePage() {
 	return (
-		<div className="w-screen h-screen p-10">
-			{
-				WISHLIST.map((wish, key) => (
-					<Card
-						key={`wish-${key}`}
-						{...{wish}}
-					/>
-				))
-			}
-		</div>
+		<MainLayout>
+			<div className="w-full h-full grid md:grid-cols-3 gap-0.5 sm:grid-cols-1 lg:grid-cols-4">
+				{
+					WISHLIST.map((wish, key) => (
+						<Card
+							key={`wish-${key}`}
+							{...{wish}}
+						/>
+					))
+				}
+			</div>
+		</MainLayout>
 	)
 }
 
