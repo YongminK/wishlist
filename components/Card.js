1de59ae0237f5abe3770f17cec6c83da51970c37
e.petrova <e.petrova@int.robotbull.com>
2021-04-17 17:58:55 +0300

    remove tailwind

--- components/Card.js
+++ components/Card.js
@@ -3,23 +3,7 @@ const Card = ({wish}) => {
 	const {title, desc, link, linkDesc = "Ссылка", img, price, status, date} = wish;
 	console.log(img)
 	return (
-		<div
-			className="w-auto h-80 shadow-md rounded-md p-4 m-6">
-			{img ?
-				<div className="flex justify-center mb-1">
-					<img
-						src={img}
-						alt={title}
-						className="block mx-auto h-32 flex-shrink"
-					/>
-				</div>
-
-			:
-				<div className="mb-1 block mx-auto h-24 sm:mx-0 sm:flex-shrink-0 bg-magic-wand bg-no-repeat bg-center"/>
-			}
-			{link && <a href={link} target="_blank" className="text-blue-700 text-xs">{linkDesc}</a>}
-			<p className="font-semibold text-lg">{title}</p>
-			<p className="text-sm">{desc}</p>
+		<div>
 		</div>
 	)
 }
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,9 @@
-import Card from "../components/Card";
-import {WISHLIST} from "../wishlist/wishes";
 import MainLayout from "../layouts/MainLayout/MainLayoyt";
 
 function HomePage() {
 	return (
 		<MainLayout>
-			<div className="w-full h-full grid md:grid-cols-3 gap-0.5 sm:grid-cols-1 lg:grid-cols-4">
-				{
-					WISHLIST.map((wish, key) => (
-						<Card
-							key={`wish-${key}`}
-							{...{wish}}
-						/>
-					))
-				}
-			</div>
+
 		</MainLayout>
 	)
 }
