0dc60eb098dae41e3a1515eb12f8c6e4241b81ae
e.petrova <e.petrova@int.robotbull.com>
2021-06-13 19:15:33 +0300

    фикс интеграции названия вкладки

--- pages/[user].js
+++ pages/[user].js
@@ -1,17 +1,24 @@
 import React from "react";
 import MainLayoyt from "layouts/MainLayout/MainLayoyt";
 import {useQuery} from "@apollo/client";
-import {GET_USER_NAME} from "graphql/user/getUserName";
+import {GET_USER_NAME} from "graphql/user/query/getUserName";
 import UserProfile from "views/user/Profile";
 import {withApollo} from "config/apolloConfig";
+import {useRouter} from "next/router";
 
 const UserPage = () => {
     // const loading = false, error = false, data = {
     //     user: {userName: 'test'}
     // }
-    const {data, loading, error} = useQuery(GET_USER_NAME)
+    const router = useRouter();
+    const {id} = router.query;
+    const {data, loading, error} = useQuery(GET_USER_NAME, {
+        variables: {
+            userId: id
+        }
+    })
     return (
-        <MainLayoyt title={`${loading || error ? "Страница пользователя" : data.user.userName} | Профиль`}>
+        <MainLayoyt title={`${loading || error ? "Страница пользователя" : data.user.nickname} | Профиль`}>
             <UserProfile/>
         </MainLayoyt>
     )
