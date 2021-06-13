2b3211f3c61025b79575a3da589d9cf8520b5d0b
e.petrova <e.petrova@int.robotbull.com>
2021-06-13 17:04:16 +0300

    получение имени пользователя

---
+++ graphql/user/getUserName.js
@@ -0,0 +1,10 @@
+import {gql} from "@apollo/client";
+
+export const GET_USER_NAME = gql `
+    query getUserName($userId: ID!) {
+        user(userId: $userId) {
+            id
+            userName
+        }
+    }
+`
