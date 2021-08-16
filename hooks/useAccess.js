12a1650477fd92ffc732f4c1debc521b6441ca10
e.petrova <e.petrova@int.robotbull.com>
2021-08-16 16:15:52 +0300

    check if is my page

--- hooks/useAccess.js
+++ hooks/useAccess.js
@@ -1,14 +1,20 @@
 import React, {useEffect, useState} from "react"
 import {useQuery} from "@apollo/client";
 import {GET_ME} from "graphql/me/getMe";
+import {useRouter} from "next/router";
 
 export const useAccess = () => {
     const [myId, setMyId] = useState('')
+    const [isMe, setIsMe] = useState(false)
     const [isLoaded, setIsLoaded] = useState(false)
+    const router = useRouter()
+    const {query} = router
 
     const {data} = useQuery(GET_ME);
     const define = (values, id) => {
         setMyId(id)
+        if (router.route === '/user')
+            setIsMe(query?.id === id)
         setIsLoaded(true)
     }
 
@@ -21,7 +27,7 @@ export const useAccess = () => {
         }
     }, [data])
 
-    return {isLoaded, isCurrentUser, define, myId}
+    return {isLoaded, isCurrentUser, define, myId, isMe}
 }
 
 export const Show = ({show, children, text = "Нет доступа", style = {fontSize: 20, textAlign: "center"}}) => {
